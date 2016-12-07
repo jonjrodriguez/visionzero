c4f7432410b2c0e0625ff9ae4e8373aeeee704f9
Nissan Azizov <nissan.azizov@nyu.edu>
2016-12-06 21:14:22 -0500

    Updated ETL for Collisions
    Now working properly with both input formats, removed irrelevant fields

--- etl/collisions/src/EtlMapper.java
+++ etl/collisions/src/EtlMapper.java
@@ -1,4 +1,3 @@
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -6,6 +5,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Calendar;
 
 public class EtlMapper extends Mapper<LongWritable, Text, NullWritable, Text>
@@ -50,61 +50,75 @@ public class EtlMapper extends Mapper<LongWritable, Text, NullWritable, Text>
         Calendar newDataFormatDate = Calendar.getInstance();
         newDataFormatDate.set(2014, 02, 1);
         Calendar fileDate = Calendar.getInstance();
-        fileDate.set(Integer.parseInt(year), Integer.parseInt(month), 1);
+        fileDate.set(Integer.parseInt(year), Integer.parseInt(month) - 1, 1);
 
+        String delim = ",";
+        // Borough, Month and Year
+  	  	sb.append(borough).append(delim).append(month).append(delim).append(year);
+  	  	
         // If the file's date is before March 2014, data is using the old format
         // Else the data is using the new format
         if (fileDate.before(newDataFormatDate)) {
-        	// TODO: Figure out the best way to parse and clean the old formatted data
-        	/*
-        	if (columns.length == 8) {
-        		String delim = ",";
+        	if (!line.contains("Address") && ((columns.length == 10) || (columns.length == 9))) {
+            	String[] injuredColumns = columns[6].split(";");
+            	String[] killedColumns = columns[7].split(";");
+        		
+            	// Precinct
+            	columns[0] = columns[0].replaceAll("[^0-9?!\\.]","");
+            	columns[0] = String.format("%03d", Integer.parseInt(columns[0]));
+            	sb.append(delim).append(columns[0]);
+            	// CollisionCount
+            	sb.append(delim).append(columns[2]);
+            	// CollisionInjuredCount
+            	if (injuredColumns.length == 5) {
+            		sb.append(delim).append(injuredColumns[4]);
+            	}
+            	else {
+            		sb.append(delim).append("0");
+            	}
+            	// CollisionKilledCount
+            	if (killedColumns.length == 5) {
+            		sb.append(delim).append(killedColumns[4]);
+            	}
+            	else {
+            		sb.append(delim).append("0");
+            	}
+            	
+            	// Motorists, Passengers, Cyclists, Pedestrians injured and killed
+            	for (int i = 0; i < 4; i++) {
+            		if ((injuredColumns.length == 5) && (killedColumns.length == 5)) {
+            			sb.append(delim).append(injuredColumns[i]);
+            		}
+            		else {
+            			sb.append(delim).append("0");
+            		}
+            	}
+            	
         	}
         	else {
         		return;
         	}
-
-        	context.write(NullWritable.get(), new Text(sb.toString()));
-        	*/
-        	return;
         }
         else {
           if (!line.contains("Collision") && (columns.length == 23))  {
-            String delim = ",";
-            sb.append(borough).append(delim).append(month).append(delim).append(year);
-            int[] ignoredColumns = {1, 2, 3, 7, 10};
+        	  Integer[] ignoredColumns = {1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 21, 22};
 
-            int i = 0;
-	          for (String col : columns) {
-              if (ArrayUtils.contains(ignoredColumns, i)) {
-                sb.append(delim).append(col);
-             		i++;
-            	}
-            }
+        	  int i = 0;
+        	  for (String col : columns) {
+        		  if (!Arrays.asList(ignoredColumns).contains(i)) {
+        			  if (i == 8) {
+        				  sb.append(delim).append("1"); 
+        			  }
+        			  sb.append(delim).append(col);
+        		  }
+        		  i++;
+        	  }
           }
           else {
-            return;
+              return;
           }
-
-    	    context.write(NullWritable.get(), new Text(sb.toString()));
-        	 if (!line.contains("Collision") && (columns.length == 23))  {
-		     String delim = ",";
-	             sb.append(borough).append(delim).append(month).append(delim).append(year);
-	             int[] ignoredColumns = {1, 2, 3, 7, 10};
-	             
-	             int i = 0;
-	             for (String col : columns) {
-	            	 if (ArrayUtils.contains(ignoredColumns, i)) {
-	            		 sb.append(delim).append(col);
-	             		 i++;
-	            	 }
-	             }
-             }
-             else {
-             	return;
-             }
-        	 
-        	 context.write(NullWritable.get(), new Text(sb.toString()));
         }
+
+    	  context.write(NullWritable.get(), new Text(sb.toString()));
     }
 }
